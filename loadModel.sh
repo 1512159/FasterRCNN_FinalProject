fd8b2ef9aaffb63c8653db77d8c47acffa3bddc8
HoangTrungHieu <hoangtrunghieu@HoangTrungHieus-MacBook-Pro.local>
2018-05-18 14:52:35 +0700

    remove cooco

--- loadModel.sh
+++ loadModel.sh
@@ -7,9 +7,10 @@ wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1G6tA
 chmod +x ~/gdrive-linux-x64
 ~/gdrive-linux-x64 download 1DkJEzW3rYScST5gFlS0UkuV3kgKFqqrj --path ~/fasterRCNN_FinalProject/tf-faster-rcnn/tools/output/res101/
 cd res101/
-tar -xvzf voc_0712_80k-110k.tgz 
 cd voc_2007_trainval+voc_2012_trainval/
+cd ~/fasterRCNN_FinalProject/tf-faster-rcnn/tools/output/res101
 mkdir default
-mv res101_faster_rcnn_iter_110000.* default/
-cd ~/fasterRCNN_FinalProject/tf-faster-rcnn/lib/
+mv voc_0712_80k-110k.tgz default
+cd default
+tar -xvzf voc_0712_80k-110k.tgz
 make
