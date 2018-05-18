6844f088a8de70d9a6be2f42c6dc003e172dfda6
HoangTrungHieu <hoangtrunghieu@HoangTrungHieus-MacBook-Pro.local>
2018-05-19 01:14:01 +0700

    fixed

--- tf-faster-rcnn/lib/roi_data_layer/roidb.py
+++ tf-faster-rcnn/lib/roi_data_layer/roidb.py
@@ -29,7 +29,7 @@ def prepare_roidb(imdb):
          for i in range(imdb.num_images)]
   for i in range(len(imdb.image_index)):
     print(i,imdb.image_path_at(i))
-    roidb[i]['image'] = imdb.image_path_at(i)
+    # roidb[i]['image'] = imdb.image_path_at(i)
     if not (imdb.name.startswith('coco')):
       roidb[i]['width'] = sizes[i][0]
       roidb[i]['height'] = sizes[i][1]
