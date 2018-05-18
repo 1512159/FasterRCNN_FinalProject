9ea741169cb3e8ad41f3c931b5f9b93f7096f4c1
HoangTrungHieu <hoangtrunghieu@HoangTrungHieus-MacBook-Pro.local>
2018-05-19 00:22:29 +0700

    fixed

--- tf-faster-rcnn/lib/roi_data_layer/minibatch.py
+++ tf-faster-rcnn/lib/roi_data_layer/minibatch.py
@@ -19,6 +19,7 @@ from utils.blob import prep_im_for_blob, im_list_to_blob
 def get_minibatch(roidb, num_classes):
   """Given a roidb, construct a minibatch sampled from it."""
   num_images = len(roidb)
+  print(num_images)
   # Sample random scales to use for each image in this batch
   random_scale_inds = npr.randint(0, high=len(cfg.TRAIN.SCALES),
                   size=num_images)
