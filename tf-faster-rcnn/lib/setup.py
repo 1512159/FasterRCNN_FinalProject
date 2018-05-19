aa7c51e63f1ab9035d5d41663c7ceda4462ae111
HoangTrungHieu <hoangtrunghieu@HoangTrungHieus-MacBook-Pro.local>
2018-05-19 23:57:49 +0700

    fix detrac.py

--- tf-faster-rcnn/lib/setup.py
+++ tf-faster-rcnn/lib/setup.py
@@ -125,7 +125,7 @@ ext_modules = [
         runtime_library_dirs=[CUDA['lib64']],
         # this syntax is specific to this build system
         # we're only going to use certain compiler args with nvcc and not with gcc
-        # the implementation of this trick is in customize_compiler() below
+        # the implementation of this trick is in  customize_compiler() below
         extra_compile_args={'gcc': ["-Wno-unused-function"],
                             'nvcc': ['-arch=sm_37',
                                      '--ptxas-options=-v',
