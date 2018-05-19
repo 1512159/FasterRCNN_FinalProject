4457979bacb64d420a8a6eddb20fa47e42f4b901
HoangTrungHieu <hoangtrunghieu@HoangTrungHieus-MacBook-Pro.local>
2018-05-19 20:35:52 +0700

    remove print cmd

--- tf-faster-rcnn/tools/demo.py
+++ tf-faster-rcnn/tools/demo.py
@@ -47,7 +47,7 @@ CLASSES = ('__background__','car', 'bus', 'van', 'others')
 
 # NETS = {'vgg16': ('vgg16_faster_rcnn_iter_70000.ckpt',),'res101': ('res101_faster_rcnn_iter_110000.ckpt',)}
 NETS = {'vgg16': ('vgg16_faster_rcnn_iter_70000.ckpt',),'res101': ('res101_faster_rcnn_iter_10000.ckpt',)}
-DATASETS= {'pascal_voc': ('voc_2007_trainval',),'pascal_voc_0712': ('voc_2007_trainval+voc_2012_trainval',),'detrac':('DETRAC2015_trainval')}
+DATASETS= {'pascal_voc': ('voc_2007_trainval',),'pascal_voc_0712': ('voc_2007_trainval+voc_2012_trainval',),'detrac':('DETRAC2015_trainval',)}
 
 # def checkOverLap(a,b):
 #     val = abs(abs(a[0]-a[3]) - abs(b[0]-b[3]))
