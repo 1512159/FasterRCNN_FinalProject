9839f7bf75723a49cbefc112dd33dc7480d9016e
HoangTrungHieu <hoangtrunghieu@HoangTrungHieus-MacBook-Pro.local>
2018-05-20 18:09:00 +0700

    edit detrac.py eval

--- tf-faster-rcnn/lib/datasets/detrac_eval.py
+++ tf-faster-rcnn/lib/datasets/detrac_eval.py
@@ -140,6 +140,8 @@ def detrac_eval(detpath,
   npos = 0
   for imagename in imagenames:
     print(imagename)
+    for obj in recs[imagename]:
+        print(obj)
     R = [obj for obj in recs[imagename] if obj['name'] == classname]
     bbox = np.array([x['bbox'] for x in R])
     if use_diff:
